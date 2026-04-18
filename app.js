6a766e62189eb911ea61905a6c29f8dfcd5ffa9c
TusharChakraborty07 <chakrabortytushar684@gmail.com>
2026-04-18 22:51:09 +0530

    ejs added

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 
+const path = require("path");
+
+// Model's
+const userModel = require("./model/userModel");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
 });
 
 app.listen(3000, () => {
